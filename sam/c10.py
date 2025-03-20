293e6136df5765ae97b27bcd258cb3b5dea84aff
Vance2025 <wfzabner@outlook.com>
2025-03-20 15:38:03 +0800

    Add files via upload

---
+++ sam/c10.py
@@ -0,0 +1,17 @@
+# loop
+
+# while and for
+# for x in range(11, 1, -2):
+#    print(x,end=' | ')
+
+a = [1,2,3,4,5,6,7,8]
+
+# for i in range(1,len(a),2):
+#     print(i, end=' | ')
+
+b = a[len(a):-3:-2]
+print(b)
+
+
+
+
