eba5db9e0fbc29b70b2c41cdfe074cecff303eb4
Vance2025 <wfzabner@outlook.com>
2025-03-21 10:28:42 +0000

    new

---
+++ loops.py
@@ -0,0 +1,21 @@
+# Description: This file contains examples of loops in Python
+
+
+# 1. while
+
+# i = 0
+# while i < 3:
+#     print('meow')
+#     i += 1
+
+
+# 2. for...in range()
+
+# for i in range(3):
+#     print('meow')
+
+# 3. print() with end parameter
+print('meow\n' * 3, end='')
+
+
+
