e76be669cbd7b613a7ad2e4db52d970a5f315bec
Vance2025 <wfzabner@outlook.com>
2025-04-02 10:14:49 +0000

    new

---
+++ list_slicing_phone.py
@@ -0,0 +1,6 @@
+def main():
+    phone = "617-689-1000"
+    print(phone)
+    print(phone[0:3])  # 617
+
+main()
---
+++ tuples_location.py
@@ -0,0 +1,7 @@
+def main():
+    coordinates = (37.7749, -122.4194)  # tuple
+    latitude, longitude = coordinates  # tuple unpacking
+    print(f'Latitude: {latitude}, Longitude: {longitude}')
+
+main()
+
